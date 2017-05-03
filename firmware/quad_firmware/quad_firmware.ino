a5afadcffd020aa077e2b7dab2a5031551b5d1c5
atorr <Alexorr.CSE@gmail.com>
2017-05-02 22:59:01 -0700

    adding some PID code to quad firmware and added the AHRS lib

--- firmware/quad_firmware/quad_firmware.ino
+++ firmware/quad_firmware/quad_firmware.ino
@@ -1,3 +1,10 @@
+#include <Adafruit_LSM9DS1.h>
+
+#include <Adafruit_Sensor_Set.h>
+#include <Adafruit_Simple_AHRS.h>
+#include <Madgwick.h>
+#include <Mahony.h>
+
 #include "radio.h"
 
 const int MOTOR1 = 8; // B5
@@ -5,10 +12,10 @@ const int MOTOR2 = 3; // E3
 const int MOTOR3 = 4; // E4
 const int MOTOR4 = 5; // E5
 
-int thr = 0;
-int yaw = 0;
-int pitch = 0;
-int roll = 0;
+float thr = 0;
+float yaw = 0;
+float pitch = 0;
+float roll = 0;
 
 typedef struct {
   int header;
@@ -18,6 +25,13 @@ typedef struct {
   int roll;
 } Control;
 Control controls;
+
+sensors_vec_t *orientation;
+sensor_t acc;
+sensor_t mag;
+Adafruit_LSM9DS1::Sensor *accelerometer;
+Adafruit_LSM9DS1::Sensor *magnetometer;
+Adafruit_Simple_AHRS ahrs (accelerometer, magnetometer);  
 
 void setup()
 {
@@ -31,6 +45,12 @@ void setup()
   analogWrite(MOTOR2, 0);
   analogWrite(MOTOR3, 0);
   analogWrite(MOTOR4, 0);
+
+  acc.type = SENSOR_TYPE_ACCELEROMETER; //TODO: USE ENUM????
+  accelerometer->getSensor(&acc);
+
+  mag.type = SENSOR_TYPE_MAGNETIC_FIELD;
+  magnetometer->getSensor(&mag);
 }
 
 void getRF()
@@ -53,9 +73,19 @@ void getRF()
   }
 }
 
+void PID(){
+  ahrs.getOrientation(orientation);
+  Serial.println(orientation->roll);
+  Serial.println(orientation->pitch);
+  Serial.println(orientation->heading);
+  
+    
+}
+
 void loop()
 {
   getRF();
+  PID();
   analogWrite(MOTOR1, thr);
   analogWrite(MOTOR2, thr);
   analogWrite(MOTOR3, thr);
